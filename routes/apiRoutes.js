7c54b0b8169c56180544eb8ff3e4f16c50c4914d
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-07-12 20:54:41 -0400

    added server.js to repo

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,4 +5,11 @@ const db = require('../db/db.json');
 module.exports = (app) => {
     // Returns db.json for the api notes path
     app.get('/api/notes', (req, res) => res.json(db));
+    // Posts new notes to db.json using the same path
+    app.post('/api/notes', (req, res) => {
+        // Creates an object from user input
+        const newNote = req.body;
+        // Saves the note as a new entry in db.json
+        db[`${newNote.title}`] = `${newNote.text}`;
+    })
 }
---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+const express = require('express');
+
+// Creates the Express app and defines the port
+const app = express();
+const PORT = 5000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// Starts the App Listener
+app.listen(PORT, () => console.log("Server running on port: ", PORT));
